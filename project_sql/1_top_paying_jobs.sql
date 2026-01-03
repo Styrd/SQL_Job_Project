ce8bbb9e376727cb71a0d500d52666c2e5eb5b69
Christoffer Lövstrand <clovstrand@gmail.com>
2026-01-03 17:47:50 +0100

    push

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,8 +1,8 @@
 /*
-Fråga: Vad är de bäst betalande jobben som Data/Business Analyst i Sverige?
-- Identifiera de 10 högst betalande jobben som Data/Business Analyst som är tillgängliga i Sverige.
-- Fokusera på jobb som har en lön specifierad (ta bort NULL).
-- Varför? Få fram bäst betalda jobb inom Data/Businesss Analyst, som kan erbjuda insikt i marknaden i Sverige.
+QUESTION: What are the top-paying Data/Business Analyst jobs in Sweden?
+- Identify the top 10 highest-paying Data/Business Analyst roles that are available in Sweden.
+- Focus on job postings with specified salaries (no NULL values)
+- Why? Aims to highlight the top-paying opportunities for Data Analysts, offering insights into employment options in Sweden.
 */
 
 SELECT
@@ -16,28 +16,28 @@ SELECT
     jpf.job_posted_date AS publicerad
 FROM
     job_postings_fact AS jpf
-LEFT JOIN                                               -- Länkar samman company_dim för att kunna få ut company_name
+LEFT JOIN                                               -- JOINs company_dim to be able to get the company name
     company_dim AS cd ON
     jpf.company_id = cd.company_id
 WHERE
-    jpf.job_country = 'Sweden' AND                      -- Filtrerar på jobb i Sverige
+    jpf.job_country = 'Sweden' AND                      -- Filter the country to 'Sweden'
     (jpf.job_title LIKE '%Data Analyst' OR
-    jpf.job_title LIKE '%Business Analyst') AND         -- Filtrar ut jobb som innehåller 'Data Analyst' eller 'Business Analyst'
-    jpf.salary_year_avg IS NOT NULL                     -- Tar bort NULL värden
+    jpf.job_title LIKE '%Business Analyst') AND         -- Filter jobs to only include 'Data Analyst' or 'Business Analyst'
+    jpf.salary_year_avg IS NOT NULL                     -- Removes NULL values
 ORDER BY
     jpf.salary_year_avg DESC
 LIMIT 10;
 
 /*
-Notering: Vi har inte tillräckligt med data för att använda denna eftersom vi är dåliga på att lista lön i Sverige, om vi ska använda Sverige som parameter så måste vi hitta andra ingångsvärden. 
-Vi bör därför skapa en ny frågeställning kring jobb Remote.
+Note: We don't have enough data, we only have 7 job listings that includes a salary in Sweden, therefore we need other parameters if we want to analyse job postings in Sweden.
+Therefore we will change the scope to remote jobs and make a new query below.
 */
 
 /*
-Fråga: Vad är de bäst betalande jobben som Data/Business Analyst Remote?
-- Identifiera de 10 högst betalande jobben som Data/Business Analyst som är tillgängliga Remote.
-- Fokusera på jobb som har en lön specifierad (ta bort NULL).
-- Varför? Få fram bäst betalda jobb inom Data/Businesss Analyst, som kan erbjuda insikt om marknaden.
+QUESTION: What are the top-paying Data/Business Analyst jobs?
+- Identify the top 10 highest-paying Data/Business Analyst roles that are available available remotely.
+- Focus on job postings with specified salaries (no NULL values)
+- Why? Aims to highlight the top-paying opportunities for Data Analysts, offering insights into employment options and location flexibility.
 */
 
 SELECT
@@ -50,14 +50,14 @@ SELECT
     jpf.job_posted_date AS publicerad
 FROM
     job_postings_fact AS jpf
-LEFT JOIN                                               -- Länkar samman company_dim för att kunna få ut company_name
+LEFT JOIN                                               -- JOINs company_dim to be able to get the company name
     company_dim AS cd ON
     jpf.company_id = cd.company_id
 WHERE
     (jpf.job_title LIKE '%Data Analyst' OR
-    jpf.job_title LIKE '%Business Analyst') AND         -- Filtrar ut jobb som innehåller 'Data Analyst' eller 'Business Analyst'
+    jpf.job_title LIKE '%Business Analyst') AND         -- Filter jobs to only include 'Data Analyst' or 'Business Analyst'
     jpf.job_location = 'Anywhere' AND                   -- Filtering on roles that are available 'Anywhere' AKA remote
-    jpf.salary_year_avg IS NOT NULL                     -- Tar bort NULL värden
+    jpf.salary_year_avg IS NOT NULL                     -- Removes NULL values
 ORDER BY
     jpf.salary_year_avg DESC
 LIMIT 10;
